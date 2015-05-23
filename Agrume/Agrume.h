3f1b5c7ed7ceb35349a84750e296fa1473540dc4
Jan Gorman <gorman.jan@gmail.com>
2015-05-23 18:20:00 +0200

    Initial commit

---
+++ Agrume/Agrume.h
@@ -0,0 +1,19 @@
+//
+//  Agrume.h
+//  Agrume
+//
+//  Created by Jan Gorman on 23/05/15.
+//  Copyright (c) 2015 Schnaub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Agrume.
+FOUNDATION_EXPORT double AgrumeVersionNumber;
+
+//! Project version string for Agrume.
+FOUNDATION_EXPORT const unsigned char AgrumeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Agrume/PublicHeader.h>
+
+
