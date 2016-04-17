72652ffb3f97737c4fcd9b4d760b3b8f1b6aa81a
Jan Gorman <gorman.jan@gmail.com>
2016-04-17 17:34:17 +0200

    Cleanup, remove unused controller

--- Example/Agrume Example/AgrumeNavigationController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AgrumeNavigationController.swift
-//  Agrume Example
-//
-//  Created by Jan GORMAN on 17/04/2016.
-//  Copyright © 2016 Schnaub. All rights reserved.
-//
-
-import Foundation
