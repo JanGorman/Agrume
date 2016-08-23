f8dc63f59a06cb2ed9f9519e82c82815abfd4143
broadwaylamb <seergy_pro@me.com>
2016-08-23 21:32:38 +0300

    Migrate Example project and tests suite to Swift 3

--- Example/Agrume Example/SingleImageViewController.swift
+++ Example/Agrume Example/SingleImageViewController.swift
@@ -8,7 +8,7 @@ import Agrume
 
 final class SingleImageViewController: UIViewController {
 
-    @IBAction func openImage(sender: AnyObject) {
+    @IBAction func openImage(_ sender: AnyObject) {
         let agrume = Agrume(image: UIImage(named: "MapleBacon")!)
         agrume.showFrom(self)
     }
--- Example/Agrume Example/SingleURLViewController.swift
+++ Example/Agrume Example/SingleURLViewController.swift
@@ -8,9 +8,9 @@ import Agrume
 
 final class SingleURLViewController: UIViewController {
 
-    @IBAction func openURL(sender: AnyObject) {
-        let agrume = Agrume(imageURL: NSURL(string: "https://dl.dropboxusercontent.com/u/512759/MapleBacon.png")!,
-                backgroundBlurStyle: .Light)
+    @IBAction func openURL(_ sender: AnyObject) {
+        let agrume = Agrume(imageURL: URL(string: "https://dl.dropboxusercontent.com/u/512759/MapleBacon.png")!,
+                backgroundBlurStyle: .light)
         agrume.showFrom(self)
     }
 
