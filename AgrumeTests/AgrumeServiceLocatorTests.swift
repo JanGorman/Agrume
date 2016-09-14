c3ab53c0f9436ce7a07ecd0591972d26f6d918c6
Jan Gorman <gorman.jan@gmail.com>
2016-09-14 07:50:41 +0200

    Fix test case

--- AgrumeTests/AgrumeServiceLocatorTests.swift
+++ AgrumeTests/AgrumeServiceLocatorTests.swift
@@ -13,7 +13,7 @@ class AgrumeServiceLocatorTests: XCTestCase {
   override func setUp() {
     super.setUp()
     
-    agrume = Agrume(imageURL: URL(string: "https://dl.dropboxusercontent.com/u/512759/MapleBacon.png")!)
+    agrume = Agrume(imageUrl: URL(string: "https://dl.dropboxusercontent.com/u/512759/MapleBacon.png")!)
   }
   
   override func tearDown() {
