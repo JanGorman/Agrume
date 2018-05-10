9a2f6359fb9158aa00c5cf928d9268e1e4509a7a
Jan Gorman <gorman.jan@gmail.com>
2018-05-10 16:59:55 +0200

    Animated gif support for remote images

--- Agrume/ImageDownloader.swift
+++ Agrume/ImageDownloader.swift
@@ -3,15 +3,14 @@
 //
 
 import UIKit
+import ImageIO
+import MobileCoreServices
+import SwiftyGif
 
 final class ImageDownloader {
 
   static func downloadImage(_ url: URL, completion: @escaping (_ image: UIImage?) -> Void) -> URLSessionDataTask? {
-    var configuration = URLSessionConfiguration.default
-    if #available(iOS 11.0, *) {
-      configuration.waitsForConnectivity = true
-    }
-    let session = URLSession(configuration: configuration)
+    let session = URLSession(configuration: newConfiguration())
     let task = session.dataTask(with: url) { data, _, error in
       var image: UIImage?
       defer {
@@ -20,10 +19,28 @@ final class ImageDownloader {
         }
       }
       guard let data = data, error == nil else { return }
-      image = UIImage(data: data)
+      if isAnimatedImage(data) {
+        image = UIImage(gifData: data)
+      } else {
+        image = UIImage(data: data)
+      }
     }
     task.resume()
     return task
+  }
+  
+  private static func newConfiguration() -> URLSessionConfiguration {
+    var configuration = URLSessionConfiguration.default
+    if #available(iOS 11.0, *) {
+      configuration.waitsForConnectivity = true
+    }
+    return configuration
+  }
+  
+  private static func isAnimatedImage(_ data: Data) -> Bool {
+    guard let imageSource = CGImageSourceCreateWithData(data as CFData, nil),
+          let imageType = CGImageSourceGetType(imageSource) else { return false }
+    return UTTypeConformsTo(imageType, kUTTypeGIF)
   }
 
 }
